71b5777ed62f0372c391b94d5fb5dcece6054634
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2021-03-23 02:38:40 +0800

    fix deform decoder resize output

--- model/two_step/deform_decoder.py
+++ model/two_step/deform_decoder.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class DeformDecoder(nn.Module):
     def __init__(self, feature_dim: int, vertex_num: int):
         super().__init__()
+        self.vertex_num = vertex_num
         self.fc = nn.Sequential(
             nn.Linear(feature_dim, 1024),
             nn.LeakyReLU(),
@@ -17,5 +18,5 @@ class DeformDecoder(nn.Module):
     def forward(self, global_features, local_features):
         features = torch.cat([global_features, local_features], 1)
         out = self.fc(features)
-        deform = out.view(-1, 128, 3) * 0.1
+        deform = out.view(-1, self.vertex_num, 3) * 0.1
         return deform
