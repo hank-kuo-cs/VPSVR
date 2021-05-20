4fd2ff8ce4d1a7ff782804ca0a693926f903fa19
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2021-05-20 17:49:04 +0800

    fix gcn bottleneck

--- model/two_step/gcn/bottleneck.py
+++ model/two_step/gcn/bottleneck.py
@@ -5,10 +5,9 @@ from torch_geometric.nn import GCNConv
 class GResBlock(nn.Module):
     def __init__(self, in_dim, hidden_dim):
         super().__init__()
-
         self.conv1 = GCNConv(in_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, in_dim)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
 
     def forward(self, inputs, edges):
         x = self.relu(self.conv1(inputs, edges))
@@ -20,16 +19,18 @@ class GResBlock(nn.Module):
 class GBottleneck(nn.Module):
     def __init__(self, in_dim, hidden_dim, out_dim, block_num: int = 6):
         super().__init__()
+
         resblock_layers = [GResBlock(in_dim=hidden_dim, hidden_dim=hidden_dim) for _ in range(block_num)]
-        self.blocks = nn.Sequential(*resblock_layers)
+        self.blocks = nn.ModuleList(resblock_layers)
 
         self.conv1 = GCNConv(in_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, out_dim)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
 
     def forward(self, inputs, edges):
         x = self.relu(self.conv1(inputs, edges))
-        x_hidden = self.blocks(x, edges)
-        x_out = self.conv2(x_hidden, edges)
+        for block in self.blocks:
+            x = block(x, edges)
+        x_out = self.conv2(x, edges)
 
-        return x_out, x_hidden
+        return x_out
