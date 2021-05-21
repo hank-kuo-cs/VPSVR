16874a6f03d8189f2755fbcf582e6a7a8c342b29
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2021-05-22 00:29:06 +0800

    deform regularization loss & tune hyperparameters

--- train.py
+++ train.py
@@ -36,21 +36,22 @@ def parse_arguments():
     parser.add_argument('--lr_translate_de', type=float, default=1e-4, help='learning rate of translate decoder')
     parser.add_argument('--lr_volume_rotate_de', type=float, default=1e-4, help='learning rate of volume rotate decoder')
     parser.add_argument('--lr_deform_de', type=float, default=1e-4, help='learning rate of deformation decoder')
-    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 of Adam optimizer')
+    parser.add_argument('--beta1', type=float, default=0.7, help='beta1 of Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.9, help='beta2 of Adam optimizer')
     parser.add_argument('--w_decay', type=float, default=0.0, help='weight decay of Adam optimizer')
 
     # Loss weight
     parser.add_argument('--l_depth', type=float, default=1.0, help='lambda of depth mse loss')
-    parser.add_argument('--l_vpdiv', type=float, default=0.5, help='lambda of vp diverse loss')
+    parser.add_argument('--l_vpdiv', type=float, default=10.0, help='lambda of vp diverse loss')
     parser.add_argument('--l_vp_cd', type=float, default=1.0, help='lambda of global vp reconstruct cd loss')
-    parser.add_argument('--l_part_cd', type=float, default=0.0, help='lambda of part vp reconstruct cd loss')
-    parser.add_argument('--l_deform_cd', type=float, default=1.0, help='lambda of deformed mesh reconstruct cd loss')
-    parser.add_argument('--l_part_deform_cd', type=float, default=0.0, help='lambda of part deformed mesh reconstruct cd loss')
-    parser.add_argument('--l_lap', type=float, default=0.0, help='lambda of laplacian regularization')
+    parser.add_argument('--l_part_vp_cd', type=float, default=0.0, help='lambda of part vp reconstruct cd loss')
+    parser.add_argument('--l_mesh_cd', type=float, default=1.0, help='lambda of deformed mesh reconstruct cd loss')
+    parser.add_argument('--l_part_mesh_cd', type=float, default=0.0, help='lambda of part deformed mesh reconstruct cd loss')
+    parser.add_argument('--l_lap', type=float, default=0.1, help='lambda of laplacian regularization')
     parser.add_argument('--l_normal', type=float, default=0.0, help='lambda of normal loss')
     parser.add_argument('--l_sobel', type=float, default=0.0, help='lambda of sobel regularization loss')
-    parser.add_argument('--l_vp_center', type=float, default=0.0, help='lambda of vp center loss')
+    parser.add_argument('--l_vp_center', type=float, default=0.01, help='lambda of vp center loss')
+    parser.add_argument('--l_deform', type=float, default=0.001, help='lambda of deformation regularization loss')
     parser.add_argument('--vpdiv_w1', type=float, default=0.01, help='w1 of cd loss of vp diverse loss')
     parser.add_argument('--gt_sample_num', type=int, default=4096, help='number of gt sample points')
 
@@ -66,7 +67,7 @@ def parse_arguments():
     # Record Setting
     parser.add_argument('--output_path', type=str, default='./output/train')
     parser.add_argument('--checkpoint_path', type=str, default='./checkpoint')
-    parser.add_argument('--record_batch_interval', type=int, default=200, help='record prediction result every N batch')
+    parser.add_argument('--record_batch_interval', type=int, default=20, help='record prediction result every N batch')
     parser.add_argument('--checkpoint_epoch_interval', type=int, default=5, help='record model checkpoint every N epoch')
 
     return parser.parse_args()
@@ -199,17 +200,19 @@ def train(args):
     azims = [45.0 * i for i in range(8)]
 
     epoch_train_losses = {'depth': [], 'vp_cd': [], 'vp_div': [], 'part_vp_cd': [],
-                          'deform_cd': [], 'part_deform_cd': [], 'lap': [], 'normal': [], 'sobel': [], 'center': []}
+                          'mesh_cd': [], 'part_mesh_cd': [],
+                          'lap': [], 'normal': [], 'sobel': [], 'center': [], 'deform': []}
 
     for epoch in range(args.epochs):
         n = 0
         avg_losses = {'depth': 0.0, 'vp_cd': 0.0, 'vp_div': 0.0, 'part_vp_cd': 0.0,
-                      'deform_cd': 0.0, 'part_deform_cd': 0.0, 'lap': 0.0, 'normal': 0.0, 'sobel': 0.0, 'center': 0.0}
+                      'mesh_cd': 0.0, 'part_mesh_cd': 0.0,
+                      'lap': 0.0, 'normal': 0.0, 'sobel': 0.0, 'center': 0.0, 'deform': []}
 
         progress_bar = tqdm(dataloader)
 
         for data in progress_bar:
-            # Load data
+            """Load Data"""
             rgbs, masks = data['rgb'].cuda(), data['mask'].cuda()  # (B, 3, H, W), (B, 1, H, W)
             vertices = [one_vertices.cuda() for one_vertices in data['vertices']]  # list((N1, 3), ..., (Nb, 3))
             faces = [one_faces.cuda() for one_faces in data['faces']]  # list((F1, 2), ..., (FB, 2))
@@ -217,14 +220,17 @@ def train(args):
             gt_depths = DepthRenderer.render_depths_of_multi_meshes(vertices, faces)
             gt_meshes = Meshing.meshing_vertices_faces(vertices, faces)
             gt_points = Sampling.sample_mesh_points(gt_meshes, sample_num=args.gt_sample_num)
+            """Load Data"""
 
+            """Depth Estimation"""
             rgbs = rgbs * masks
             pred_depths = depth_ae(rgbs)
             pred_depths = pred_depths * masks
 
             depth_mse_loss = mse_loss_func(pred_depths, gt_depths) * args.l_depth
+            """Depth Estimation"""
 
-            # VP Diverse Loss
+            """Volumetric Primitive Prediction"""
             input_depths = pred_depths if torch.rand((1,)).item() > depth_tf else gt_depths
             global_features, perceptual_feature_list = depth_en(input_depths)
 
@@ -233,7 +239,6 @@ def train(args):
             vp_div_loss, _, vp_indices = cd_loss_func(vp_centers, gt_points, w1=args.vpdiv_w1)
             vp_div_loss *= args.l_vpdiv
 
-            # CD loss
             volumes, rotates = [], []
             vp_features = get_local_features(vp_centers, input_depths, perceptual_feature_list)
 
@@ -255,17 +260,24 @@ def train(args):
 
             part_gt_points = get_part_gt_points(gt_points, vp_indices, vp_num)
 
-            if args.l_part_cd > 0:
+            if args.l_part_vp_cd > 0:
                 for i in range(vp_num):
                     part_pred_coarse_points = pred_coarse_points[:, i * part_point_num: (i + 1) * part_point_num, :]
-                    part_vp_cd_loss += cd_loss_func(part_pred_coarse_points, part_gt_points[i])[0] * args.l_part_cd
+                    part_vp_cd_loss += cd_loss_func(part_pred_coarse_points, part_gt_points[i])[0] * args.l_part_vp_cd
             part_vp_cd_loss /= vp_num
 
+            vp_recon_loss = vp_div_loss + vp_cd_loss + part_vp_cd_loss
+            """Volumetric Primitive Prediction"""
+
+            """Mesh Reconstruction"""
             pred_meshes = Meshing.vp_meshing(volumes, rotates, translates,
                                              cuboid_num=args.cuboid_num, sphere_num=args.sphere_num)
             vp_meshes = [TriangleMesh.from_tensors(m.vertices.clone(), m.faces.clone()) for m in pred_meshes]
 
             deformation = deform_gcn(pred_meshes, input_depths, perceptual_feature_list, global_features)
+
+            deform_loss = torch.abs(deformation).mean() \
+                if args.l_deform > 0 else torch.tensor(0.0).cuda()
 
             for b in range(len(pred_meshes)):
                 pred_meshes[b].vertices += deformation[b]
@@ -275,53 +287,62 @@ def train(args):
 
             pred_vertices = torch.cat([m.vertices[None] for m in pred_meshes])
 
-            deform_cd_loss, _, _ = cd_loss_func(pred_vertices, gt_points)
-            deform_cd_loss *= args.l_deform_cd
+            mesh_cd_loss, _, _ = cd_loss_func(pred_vertices, gt_points)
+            mesh_cd_loss *= args.l_mesh_cd
 
-            part_deform_cd_loss = torch.tensor(0.0).cuda()
+            part_mesh_cd_loss = torch.tensor(0.0).cuda()
 
-            if args.l_part_deform_cd > 0:
+            if args.l_part_mesh_cd > 0:
                 for i in range(vp_num):
                     part_pred_vertices = pred_vertices[:, i * part_point_num: (i + 1) * part_point_num, :]
-                    part_deform_cd_loss += cd_loss_func(part_pred_vertices, part_gt_points[i])[0] * args.l_part_deform_cd
-            part_deform_cd_loss /= vp_num
+                    part_mesh_cd_loss += cd_loss_func(part_pred_vertices, part_gt_points[i])[0] * args.l_part_mesh_cd
+            part_mesh_cd_loss /= vp_num
 
-            lap_loss = lap_loss_func(vp_meshes, pred_meshes) * args.l_lap if args.l_lap > 0 else torch.tensor(0.0).cuda()
-            normal_loss = normal_loss_func(pred_meshes, vertices, faces) * args.l_normal if args.l_normal > 0 else torch.tensor(0.0).cuda()
+            lap_loss = lap_loss_func(vp_meshes, pred_meshes) * args.l_lap \
+                if args.l_lap > 0 else torch.tensor(0.0).cuda()
+
+            normal_loss = normal_loss_func(pred_meshes, vertices, faces) * args.l_normal \
+                if args.l_normal > 0 else torch.tensor(0.0).cuda()
 
             pred_vertices = [m.vertices for m in pred_meshes]
             pred_faces = [m.faces for m in pred_meshes]
             pred_depths = DepthRenderer.render_depths_of_multi_meshes_with_multi_view(
                 pred_vertices, pred_faces, dists=dists, elevs=elevs, azims=azims)
 
-            sobel_loss = sobel_loss_func(pred_depths) * args.l_sobel if args.l_sobel > 0 else torch.tensor(0.0).cuda()
+            sobel_loss = sobel_loss_func(pred_depths) * args.l_sobel \
+                if args.l_sobel > 0 else torch.tensor(0.0).cuda()
 
-            vp_recon_loss = vp_div_loss + vp_cd_loss + part_vp_cd_loss
-            mesh_recon_loss = deform_cd_loss + part_deform_cd_loss + lap_loss + normal_loss + sobel_loss + center_loss
+            mesh_recon_loss = mesh_cd_loss + part_mesh_cd_loss + \
+                              lap_loss + normal_loss + sobel_loss + center_loss + deform_loss
+            """Mesh Reconstruction"""
 
+            """Optimize Model"""
             total_loss = depth_mse_loss + vp_recon_loss + mesh_recon_loss
 
             optimizer.zero_grad()
             total_loss.backward()
             optimizer.step()
+            """Optimize Model"""
 
+            """Record Step Result"""
             progress_bar.set_description(
-                'MSE = %.6f, VP = %.6f, Part VP = %.6f, Div = %.6f, '
-                'Deform = %.6f, Part Deform = %.6g, Lap = %.6f, Normal = %.6f, Sobel = %.6f, Center = %.6f'
+                'MSE = %.6f, VP CD = %.6f, Part VP CD = %.6f, VP Div = %.6f, Mesh CD = %.6f, Part Mesh CD = %.6f, '
+                'Lap = %.6f, Normal = %.6f, Sobel = %.6f, Center = %.6f, Deform = %.6f'
                 % (depth_mse_loss.item(), vp_cd_loss.item(), part_vp_cd_loss.item(), vp_div_loss.item(),
-                   deform_cd_loss.item(), part_deform_cd_loss.item(),
-                   lap_loss.item(), normal_loss.item(), sobel_loss.item(), center_loss.item()))
+                   mesh_cd_loss.item(), part_mesh_cd_loss.item(),
+                   lap_loss.item(), normal_loss.item(), sobel_loss.item(), center_loss.item(), deform_loss.item()))
 
             avg_losses['depth'] += depth_mse_loss.item()
             avg_losses['vp_cd'] += vp_cd_loss.item()
             avg_losses['vp_div'] += vp_div_loss.item()
             avg_losses['part_vp_cd'] += part_vp_cd_loss.item()
-            avg_losses['deform_cd'] += deform_cd_loss.item()
-            avg_losses['part_deform_cd'] += part_deform_cd_loss.item()
+            avg_losses['mesh_cd'] += mesh_cd_loss.item()
+            avg_losses['part_mesh_cd'] += part_mesh_cd_loss.item()
             avg_losses['lap'] += lap_loss.item()
             avg_losses['normal'] += normal_loss.item()
             avg_losses['sobel'] += sobel_loss.item()
             avg_losses['center'] += center_loss.item()
+            avg_losses['deform'] += deform_loss.item()
             n += 1
 
             if n % args.record_batch_interval == 0 and (epoch + 1) % 5 == 0:
@@ -338,16 +359,19 @@ def train(args):
                 save_vp_result(rgb=rgbs[0], mask=masks[0], input_depth=input_depths[0],
                                predict_mesh=pred_meshes[0], gt_mesh=gt_meshes[0],
                                vp_num=vp_num, vertex_num=args.vertex_num, save_path=mesh_save_path)
+            """Record Step Result"""
 
+        """Record Epoch Result"""
         for key in list(avg_losses.keys()):
             avg_losses[key] /= n
             epoch_train_losses[key].append(avg_losses[key])
 
-        print('Epoch %d avg loss: Depth MSE = %.6f, VP CD = %.6f, Part VP CD = %.6f, VP Div = %.6f, '
-              'Deform CD = %.6f, Part Deform CD = %.6f, Lap = %.6f, Normal = %.6f, Sobel = %.6f, Center = %.6f\n'
+        print('Epoch %d avg loss: '
+              'Depth MSE = %.6f, VP CD = %.6f, Part VP CD = %.6f, VP Div = %.6f, Mesh CD = %.6f, Part Mesh CD = %.6f, '
+              'Lap = %.6f, Normal = %.6f, Sobel = %.6f, Center = %.6f, Deform = %.6f\n'
               % (epoch + 1, avg_losses['depth'], avg_losses['vp_cd'], avg_losses['part_vp_cd'], avg_losses['vp_div'],
-                 avg_losses['deform_cd'], avg_losses['part_deform_cd'], avg_losses['lap'], avg_losses['normal'],
-                 avg_losses['sobel'], avg_losses['center']))
+                 avg_losses['mesh_cd'], avg_losses['part_mesh_cd'], avg_losses['lap'], avg_losses['normal'],
+                 avg_losses['sobel'], avg_losses['center'], avg_losses['deform']))
 
         if (epoch+1) % args.checkpoint_epoch_interval == 0:
             model_dict = {'depth_ae': depth_ae.state_dict(),
@@ -359,12 +383,17 @@ def train(args):
             for network_name, model_weight in model_dict.items():
                 model_path = os.path.join(checkpoint_paths[network_name], '%s_epoch%03d.pth' % (network_name, epoch+1))
                 torch.save(model_weight, model_path)
+        """Record Epoch Result"""
 
-        depth_tf *= args.dtf_decay
+        """Update Scheduler"""
         scheduler.step()
+        depth_tf *= args.dtf_decay
+        """Update Scheduler"""
 
+    """Record Training Loss Curve"""
     for key in list(epoch_train_losses.keys()):
         np.save(os.path.join(record_paths['loss'], key + '.npy'), np.array(epoch_train_losses[key]))
+    """Record Training Loss Curve"""
 
 
 if __name__ == '__main__':
