1bd68540cd197cac513e3feaf11744e71d2772c4
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2021-06-02 11:18:42 +0800

    seen class setting

--- dataset/r2n2.py
+++ dataset/r2n2.py
@@ -9,20 +9,13 @@ from torch.utils.data import Dataset
 from .base import transform_image
 
 
-train_classes = ['02691156', '02958343', '03001627']
-test_classes = ['02828884', '03211117', '03636649', '03691459', '02933112'
-                '04090263', '04256520', '04379243', '04401088', '04530566']
-
-CLASSES = {'train': train_classes, 'test': test_classes}
-
-
 class R2N2Dataset(Dataset):
     def __init__(self, args, dataset_type):
         super().__init__()
 
         self.args = args
         self.dataset_type = dataset_type
-        self.split_data = {}
+        self.p2m_data = []
         self.samples = []
 
         self._load_data()
@@ -48,41 +41,38 @@ class R2N2Dataset(Dataset):
     def _load_data(self):
         self._load_split_data()
 
-        dataset_indices = self.split_data[self.dataset_type]
-        obj_num_each_class = self.args.size // len(CLASSES[self.dataset_type]) // 24
-        class_obj_num = {}
-        for class_id in CLASSES[self.dataset_type]:
-            class_obj_num[class_id] = 0
+        obj_num_each_class = self.args.size // 13 // 5
+        class_obj_nums = {}
 
-        for i in tqdm(range(len(dataset_indices))):
-            class_id, obj_id = dataset_indices[i][0], dataset_indices[i][1]
-            if class_id not in class_obj_num or class_obj_num[class_id] == obj_num_each_class:
+        for i in tqdm(range(len(self.p2m_data))):
+            class_id, obj_id = self.p2m_data[i][0], self.p2m_data[i][1]
+            if class_id not in class_obj_nums:
+                class_obj_nums[class_id] = 0
+            if class_obj_nums[class_id] == obj_num_each_class:
                 continue
 
             imgs_dir_path = os.path.join(self.args.root, class_id, obj_id, 'rendering')
-            imgs_path, azims, elevs, dists = self._load_imgs_in_one_dir(imgs_dir_path)
+            imgs_path = sorted(glob(imgs_dir_path + '/*.png'))[:5]
+            # imgs_path, _, _, _ = self._load_imgs_in_one_dir(imgs_dir_path)
 
-            meshs_path = sorted(glob(os.path.join(self.args.root, class_id, obj_id, 'objs') + '/*.obj'))
+            meshes_path = sorted(glob(os.path.join(self.args.root, class_id, obj_id, 'objs') + '/*.obj'))[:5]
+
+            if not len(meshes_path):
+                continue
 
             for j in range(len(imgs_path)):
-                self.samples.append({'img_path': imgs_path[j], 'mesh_path': meshs_path[j], 'class_id': class_id,
-                                     'dist': dists[j], 'elev': elevs[j], 'azim': azims[j]})
+                self.samples.append({'img_path': imgs_path[j], 'mesh_path': meshes_path[j], 'class_id': class_id,
+                                     'dist': 1.0, 'elev': 0.0, 'azim': 0.0})
 
-            class_obj_num[class_id] += 1
+            class_obj_nums[class_id] += 1
 
     def _load_split_data(self):
-        split_data = {}
-        str_data = open(self.args.root + '/split.csv', 'r').read()
-
-        split_data['train'] = re.findall(r'.+,(.+),.+,(.+),train', str_data)
-        split_data['test'] = re.findall(r'.+,(.+),.+,(.+),test', str_data)
-        split_data['train'].extend(re.findall(r'.+,(.+),.+,(.+),val', str_data))
-
-        self.split_data = split_data
+        str_data = open(os.path.join(self.args.root, 'p2m_%s.txt' % self.dataset_type), 'r').read()
+        self.p2m_data = re.findall(r'ShapeNetP2M/(.+)/(.+)/rendering/00.dat', str_data)
 
     def _load_imgs_in_one_dir(self, dir_path):
         meta_path = dir_path + '/rendering_metadata.txt'
-        imgs_path = sorted(glob(dir_path + '/*.png'))
+        imgs_path = sorted(glob(dir_path + '/*.png'))[:5]
         azims, elevs, dists = self._load_meta(meta_path)
 
         return imgs_path, azims, elevs, dists
@@ -100,14 +90,15 @@ class R2N2Dataset(Dataset):
     def _load_meta(meta_path: str) -> (list, list, list):
         azims, elevs, dists = [], [], []
         meta_str = open(meta_path, 'r').read()
-        meta_datas = re.findall(r'([\.0-9]+?) ([\.0-9]+?) 0 ([\.0-9]+?) 25', meta_str)
+        meta_datas = re.findall(r'([\.0-9]+?) ([\.0-9]+?) 0 ([\.0-9]+?) 25', meta_str)[:5]
         for meta_data in meta_datas:
             cameras = list(map(float, meta_data))
             cameras[2] *= 1.754
 
             azims.append(cameras[0])
             elevs.append(cameras[1])
-            dists.append(cameras[2])
+            # dists.append(cameras[2])
+            dists.append(1.0)
 
         return azims, elevs, dists
 
@@ -115,5 +106,3 @@ class R2N2Dataset(Dataset):
     def _load_vertices_and_faces(mesh_path):
         mesh = TriangleMesh.from_obj(mesh_path)
         return mesh.vertices, mesh.faces
-
-
